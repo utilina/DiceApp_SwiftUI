c9361e9894aa14c789656fe8db1d110f09190ec7
Anastasiya Ulitina <anastulitina@gmail.com>
2020-10-30 22:33:58 +0300

    Dice app interface

--- DiceApp_SwiftUI/ContentView.swift
+++ DiceApp_SwiftUI/ContentView.swift
@@ -9,13 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: 1)
+                    DiceView(n: 1)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
             .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone 11"))
     }
 }
+
+
